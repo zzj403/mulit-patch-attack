b083ebd0bebbddc86e5bd3a12af836f43f9dc4a0
zzj403 <44524875+zzj403@users.noreply.github.com>
2020-05-21 20:47:12 +0800

    Update load_data_0517.py

--- load_data_0517.py
+++ load_data_0517.py
@@ -38,7 +38,7 @@ class PatchTransformer(nn.Module):
 
     Module providing the functionality necessary to transform a list of patches, put them at the location
     defined by a list of location.
-    
+
     Output the img which is patched
 
     """
@@ -115,7 +115,7 @@ class PatchTransformer(nn.Module):
 
         # apply cut-off
         patch_affine2 = patch_affine2 * patch_mask_affine2
-        
+
         # apply patches to the image
         advs = torch.unbind(patch_affine2, 1)
         for adv in advs:
@@ -124,141 +124,7 @@ class PatchTransformer(nn.Module):
         return img_patched
 
 
-class PatchApplier(nn.Module):
-    """PatchApplier: applies adversarial patches to images.
 
-    Module providing the functionality necessary to apply a patch to all detections in all images in the batch.
-
-    """
-
-    def __init__(self):
-        super(PatchApplier, self).__init__()
-
-    def forward(self, img_batch, adv_batch):
-        advs = torch.unbind(adv_batch, 1)
-        for adv in advs:
-            # img_batch = torch.where((adv == 0), img_batch, adv)0.000001
-            img_batch = torch.where((adv == 0), img_batch, adv)  # zzj:注意存在边缘擦除的可能性
-        return img_batch
-
-'''
-class PatchGenerator(nn.Module):
-    """PatchGenerator: network module that generates adversarial patches.
-
-    Module representing the neural network that will generate adversarial patches.
-
-    """
-
-    def __init__(self, cfgfile, weightfile, img_dir, lab_dir):
-        super(PatchGenerator, self).__init__()
-        self.yolo = Darknet(cfgfile).load_weights(weightfile)
-        self.dataloader = torch.utils.data.DataLoader(InriaDataset(img_dir, lab_dir, shuffle=True),
-                                                      batch_size=5,
-                                                      shuffle=True)
-        self.patchapplier = PatchApplier()
-        self.nmscalculator = NMSCalculator()
-        self.totalvariation = TotalVariation()
-
-    def forward(self, *input):
-        pass
-'''
-
-class InriaDataset(Dataset):
-    """InriaDataset: representation of the INRIA person dataset.
-
-    Internal representation of the commonly used INRIA person dataset.
-    Available at: http://pascal.inrialpes.fr/data/human/
-
-    Attributes:
-        len: An integer number of elements in the
-        img_dir: Directory containing the images of the INRIA dataset.
-        lab_dir: Directory containing the labels of the INRIA dataset.
-        img_names: List of all image file names in img_dir.
-        shuffle: Whether or not to shuffle the dataset.
-
-    """
-
-    def __init__(self, img_dir, lab_dir, max_lab, imgsize, shuffle=True):
-        n_png_images = len(fnmatch.filter(os.listdir(img_dir), '*.png'))
-        n_jpg_images = len(fnmatch.filter(os.listdir(img_dir), '*.jpg'))
-        n_images = n_png_images + n_jpg_images
-        n_labels = len(fnmatch.filter(os.listdir(lab_dir), '*.txt'))
-        assert n_images == n_labels, "Number of images and number of labels don't match"
-        self.len = n_images
-        self.img_dir = img_dir
-        self.lab_dir = lab_dir
-        self.imgsize = imgsize
-        self.img_names = fnmatch.filter(os.listdir(img_dir), '*.png') + fnmatch.filter(os.listdir(img_dir), '*.jpg')
-        self.shuffle = shuffle
-        self.img_paths = []
-        for img_name in self.img_names:
-            self.img_paths.append(os.path.join(self.img_dir, img_name))
-        self.lab_paths = []
-        for img_name in self.img_names:
-            lab_path = os.path.join(self.lab_dir, img_name).replace('.jpg', '.txt').replace('.png', '.txt')
-            self.lab_paths.append(lab_path)
-        self.max_n_labels = max_lab
-
-    def __len__(self):
-        return self.len
-
-    def __getitem__(self, idx):
-        assert idx <= len(self), 'index range error'
-        img_path = os.path.join(self.img_dir, self.img_names[idx])
-        lab_path = os.path.join(self.lab_dir, self.img_names[idx]).replace('.jpg', '.txt').replace('.png', '.txt')
-        image = Image.open(img_path).convert('RGB')
-        if os.path.getsize(lab_path):       #check to see if label file contains data.
-            label = np.loadtxt(lab_path)
-        else:
-            label = np.ones([5])
-
-        label = torch.from_numpy(label).float()
-        if label.dim() == 1:
-            label = label.unsqueeze(0)
-
-        image, label = self.pad_and_scale(image, label)
-        transform = transforms.ToTensor()
-        image = transform(image)
-        label = self.pad_lab(label)
-        return image, label
-
-    def pad_and_scale(self, img, lab):
-        """
-
-        Args:
-            img:
-
-        Returns:
-
-        """
-        w,h = img.size
-        if w==h:
-            padded_img = img
-        else:
-            dim_to_pad = 1 if w<h else 2
-            if dim_to_pad == 1:
-                padding = (h - w) / 2
-                padded_img = Image.new('RGB', (h,h), color=(127,127,127))
-                padded_img.paste(img, (int(padding), 0))
-                lab[:, [1]] = (lab[:, [1]] * w + padding) / h
-                lab[:, [3]] = (lab[:, [3]] * w / h)
-            else:
-                padding = (w - h) / 2
-                padded_img = Image.new('RGB', (w, w), color=(127,127,127))
-                padded_img.paste(img, (0, int(padding)))
-                lab[:, [2]] = (lab[:, [2]] * h + padding) / w
-                lab[:, [4]] = (lab[:, [4]] * h  / w)
-        resize = transforms.Resize((self.imgsize,self.imgsize))
-        padded_img = resize(padded_img)     #choose here
-        return padded_img, lab
-
-    def pad_lab(self, lab):
-        pad_size = self.max_n_labels - lab.shape[0]
-        if(pad_size>0):
-            padded_lab = F.pad(lab, (0, 0, 0, pad_size), value=1)
-        else:
-            padded_lab = lab
-        return padded_lab
 
 if __name__ == '__main__':
-   pass
+    pass
